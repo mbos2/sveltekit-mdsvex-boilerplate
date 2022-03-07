240c9b65ed3abc4660a0a1765ece55004fc0e134
Matej Bošnjak <mbosnjak2@outlook.com>
2022-03-07 16:57:46 +0100

    Init

---
+++ src/components/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let count = 0;
+
+  const increment = async() => {
+    count++;
+  }
+
+</script>
+<main>
+  <button id="increment" on:click={increment}>Increment</button> <span>{count}</span>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import HelloWorld from '../svx/HelloWorld.svx';
+</script>
+
+<HelloWorld />
